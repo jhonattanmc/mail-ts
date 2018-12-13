9e16d23ef9e19b19168a5d3773603cb92bdae1b8
jmamani <jmamani@exisoft.com.ar>
2018-12-13 14:16:36 -0300

    123

---
+++ configs/configs.ts
@@ -0,0 +1,8 @@
+class Configs {
+    public host = 'aspmx.l.google.com.';
+    public port = 25;
+    // public user = "mmendoza@exisoft.com.ar";
+    // public password = "";
+}
+
+export default new Configs;
---
+++ services/mail.ts
@@ -0,0 +1,46 @@
+import * as nodemailer from "nodemailer";
+import config from '../configs/configs';
+import { MailOptions } from "nodemailer/lib/json-transport";
+
+class Mail {
+
+    constructor(
+        public to?: string,
+        public subject?: string,
+        public message?: string) { }
+
+
+    sendMail() {
+        // let mail: MailOptions;
+        let mailOptions: MailOptions = {
+            from: "mmendoza@exisoft.com.ar",
+            to: this.to,
+            subject: this.subject,
+            html: this.message
+        };
+
+        const transporter = nodemailer.createTransport({
+            host: config.host,
+            port: config.port,
+            secure: false,
+            // auth: {
+            //     user: config.user,
+            //     pass: config.password
+            // },
+            tls: { rejectUnauthorized: true }
+        });
+
+
+        console.log(mailOptions);
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                return error;
+            } else {
+                return "E-mail enviado com sucesso!" + info;
+            }
+        });
+    }
+}
+
+export default new Mail;
